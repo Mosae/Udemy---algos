61e6948cc6e18e822d8b64ade18880d18556bfb1
Mosae /Mos'eye/ <mosaelitsoane@gmail.com>
2021-02-14 03:04:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function fizzBuzz(num){
+  for(let i=1; i <= num; i++){
+    let multipleOf3 = i % 3 === 0
+    let multipleOf5 = i % 5 === 0
+  if(i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz')
+  else if(multipleOf3) console.log('Fizz')
+  else if (multipleOf5 ) console.log('Buzz')
+  else console.log(i)
+
+  }
+}
+fizzBuzz(20)
+
